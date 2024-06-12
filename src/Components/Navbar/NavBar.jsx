677804ce0ac4c6a0cea50b2df16e07b99c6d2a5e
lionelkvn <lionel.natividad@gmail.com>
2024-06-12 15:07:47 +0800

    first commit

---
+++ src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+   <div>
+        <div>
+            
+
+        </div>
+        <div>
+
+        </div>
+   </div>
+  )
+}
+
+export default NavBar
